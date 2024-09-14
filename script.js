af78c0b8ebbc3d9fe5d052c66a25db3a4fac744b
pratigya0960117 <spratigya673@gmail.com>
2024-09-14 23:30:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,40 +3,40 @@
 //     smooth: true
 // });
 // var a = document.querySelector("#elem1")
-console.log(a)
-var elemC = document.querySelector("#elem-container");
-var fixed = document.querySelector("#fixed-image");
-
-elemC.addEventListener("mouseenter", function() {
-    fixed.style.display = "block"; // Or any valid display value like "block", "inline", etc.
-});
-elemC.addEventListener("mouseleave", function() {
-    fixed.style.display = "none"; // Or any valid display value like "block", "inline", etc.
-});
-var elems = document.querySelectorAll(".elems");
+// console.log(a)
+// var elemC = document.querySelector("#elem-container");
 // var fixed = document.querySelector("#fixed-image");
 
-elems.forEach(function(e) {
-    e.addEventListener("mouseenter", function() {
-        var image = e.getAttribute("data-image");
-        fixed.style.backgroundImage = `url(${image})`; // Use backticks for template literals
-    });
-});
+// elemC.addEventListener("mouseenter", function() {
+//     fixed.style.display = "block"; // Or any valid display value like "block", "inline", etc.
+// });
+// elemC.addEventListener("mouseleave", function() {
+//     fixed.style.display = "none"; // Or any valid display value like "block", "inline", etc.
+// });
+// var elems = document.querySelectorAll(".elems");
+// // var fixed = document.querySelector("#fixed-image");
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+// elems.forEach(function(e) {
+//     e.addEventListener("mouseenter", function() {
+//         var image = e.getAttribute("data-image");
+//         fixed.style.backgroundImage = `url(${image})`; // Use backticks for template literals
+//     });
+// });
 
-  var menu = document.querySelector("nav h3")
-  var full = document.querySelector("#full-src")
-  menu.addEventListener("click",function(){
-        full.style.top = 0
-        navimg.style.opacity=0
-  });
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: "auto",
+//     centeredSlides: true,
+//     spaceBetween: 30,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+
+//   var menu = document.querySelector("nav h3")
+//   var full = document.querySelector("#full-src")
+//   menu.addEventListener("click",function(){
+//         full.style.top = 0
+//         navimg.style.opacity=0
+//   });
 
